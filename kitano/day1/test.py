5bd193a67e2d6639d7638506046d81781a002c9a
Kitanomy <kitano.hiroki.gm@mynavi.jp>
2024-05-14 05:15:59 +0000

    テスト回答

--- kitano/day1/test.py
+++ kitano/day1/test.py
@@ -1,1 +1,14 @@
-print('Hello World')
+import sys
+arg = sys.argv
+
+input1 = int(arg[1])
+input2 = int(arg[2])
+input3 = int(arg[3])
+
+print(input1 + input2 + input3, end="")
+
+
+# 1
+# name = arg[1]
+
+# print("Hello " + name + " !", end="")
