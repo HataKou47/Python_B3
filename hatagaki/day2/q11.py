099b9fbd3c7fd3b1717bd12d460e19fa3e2fe242
HataKou47 <hatagaki.koki.zv@mynavi.jp>
2024-05-15 05:58:49 +0000

    solve q11

---
+++ hatagaki/day2/q11.py
@@ -0,0 +1,14 @@
+import sys
+from decimal import Decimal, ROUND_HALF_UP
+
+args = sys.argv
+
+f_station = args[1]
+t_station = args[2]
+
+distance_dict = {'東京':0.0, '品川':6.78, '新横浜':25.54, 
+                     '名古屋':342.02, '京都':476.31, '新大阪':515.35}
+
+distance = abs(distance_dict[f_station] - distance_dict[t_station])
+
+print(Decimal(str(distance)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP), end="")
