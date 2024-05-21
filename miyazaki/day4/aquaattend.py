679adc977d8d6fec798f1e8f90042d28b3702708
TakahiroMiyazai <miyazaki.takahiro.bg@mynavi.jp>
2024-05-21 00:31:51 +0000

    add day4 +a

--- miyazaki/day4/aquaattend.py
+++ miyazaki/day4/aquaattend.py
@@ -1,13 +1,22 @@
 import sys
 from datetime import date
 from database import session
-from tables import Holiday
+from tables import Attendnum
 args = sys.argv
 
+year = int(args[1][0:4])
+month = int(args[1][4:6])
+day = int(args[1][6:8])
+dat = date(year, month, day)
+adult = int(args[2])
+child = int(args[3])
+
 # 登録するデータの編集
-holiday= Holiday(
-    holi_date = date(2023, 1, 1),
-    holi_text = "お正月"
+holiday= Attendnum(
+    date = dat
+    seq = 
+    adult = adult
+    child = child
 )
 #INSERT処理
 session.add(holiday)
